778be800ce3576aff1ac617066fc40f73740803d
Mad Mike <3929826-fluential@users.noreply.gitlab.com>
2023-06-20 13:48:56 +0100

    feat: add mono space title

--- pyro.py
+++ pyro.py
@@ -1193,7 +1193,7 @@ async def callback_answer(app, callback_query):
     if len(get_logs_history(callback_query.from_user.id)) > 0:
        snowpl = f'site:soundcloud.com {nowpl}'
 
-       await app.send_message(callback_query.from_user.id, f"👍 You #voted [{callback_query.data}] #v{callback_query.data} at {station} for 🎵 '{nowpl}'", disable_notification=False,
+       await app.send_message(callback_query.from_user.id, f"👍 You #voted [{callback_query.data}] #v{callback_query.data} at {station} for 🎵 '`{nowpl}`'", disable_notification=False,
                 reply_markup =  InlineKeyboardMarkup(
                 [
                    [  # First row
